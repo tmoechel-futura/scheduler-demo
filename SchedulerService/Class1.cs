b8178f9fab3ff3a52a6f2410954aee0aa2809842
tmoechel-futura <t.moechel@futura-solutions.de>
2026-01-11 09:04:53 +0100

    add project files

---
+++ SchedulerService/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace SchedulerService;
+
+public class Class1
+{
+}
---
+++ SimpleScheduler/Program.cs
@@ -0,0 +1,53 @@
+﻿// Grab the Scheduler instance from the Factory
+
+using Microsoft.Extensions.Logging;
+using Quartz;
+using Quartz.Impl;
+using Quartz.Logging;
+using SimpleScheduler;
+
+var loggerFactory = LoggerFactory.Create(builder =>
+{
+    builder
+        .SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Debug)
+        .AddSimpleConsole(options =>
+        {
+            options.IncludeScopes = true;
+            options.SingleLine = true;
+            options.TimestampFormat = "hh:mm:ss ";
+        });
+});
+
+LogContext.SetCurrentLogProvider(loggerFactory);
+
+StdSchedulerFactory factory = new StdSchedulerFactory();
+IScheduler scheduler = await factory.GetScheduler();
+
+// and start it off
+await scheduler.Start();
+
+// define the job and tie it to our HelloJob class
+IJobDetail job = JobBuilder.Create<HelloJob>()
+    .WithIdentity("job1", "group1")
+    .Build();
+
+// Trigger the job to run now, and then repeat every 10 seconds
+ITrigger trigger = TriggerBuilder.Create()
+    .WithIdentity("trigger1", "group1")
+    .StartNow()
+    .WithSimpleSchedule(x => x
+        .WithIntervalInSeconds(10)
+        .RepeatForever())
+    .Build();
+
+// Tell Quartz to schedule the job using our trigger
+await scheduler.ScheduleJob(job, trigger);
+
+// You could also schedule multiple triggers for the same job with
+// await scheduler.ScheduleJob(job, new List<ITrigger>() { trigger1, trigger2 }, replace: true);
+
+// some sleep to show what's happening
+await Task.Delay(TimeSpan.FromSeconds(30));
+
+// and last shut down the scheduler when you are ready to close your program
+await scheduler.Shutdown();
