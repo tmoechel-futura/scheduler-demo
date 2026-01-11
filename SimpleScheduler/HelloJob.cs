c32d37fc926b3c942ff69a804c9bc7925dd7da78
tmoechel-futura <t.moechel@futura-solutions.de>
2026-01-11 09:01:55 +0100

    Simple Scheduler

---
+++ SimpleScheduler/HelloJob.cs
@@ -0,0 +1,11 @@
+using Quartz;
+
+namespace SimpleScheduler;
+
+public class HelloJob : IJob
+{
+    public async Task Execute(IJobExecutionContext context)
+    {
+        await Console.Out.WriteLineAsync("Greetings from HelloJob!");
+    }
+}
